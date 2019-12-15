55c11bdc79d574293c6826bef3c428ad731fdede
Anthony Mai <anthony.mai530@gmail.com>
2019-12-15 01:19:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,46 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
+    @songs = []
     save
   end
-  
-  def save
-    @@all << self
-  end
-  
+
   def self.all
     @@all
   end
-  
-  def songs
-    Song.all.select{|song| song.artist == self}
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
   end
-  
-  def add_song(song)
-    song.artist = self
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
   end
-  
+
   def self.find_or_create_by_name(name)
-    if Artist.name == nil
-      name = Artist.new
-    else
-      Artist.name = name
-    end
+    self.find(name) || self.create(name)
   end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def print_songs
+    songs.each { |song| puts song.name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def songs
+    @songs
+  end
+  
+end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   attr_accessor :name, :artist
   @@all = []
+  
   def initialize(name)
     @name = name
     save
@@ -15,7 +16,16 @@ class Song
   end
   
   def artist_name=(name)
-    if Artist.name = nil
-      Artist
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
+  
+  def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+    song.artist_name = artist
+    song
+  end
   
 end
